21d7ca94938431b616277515a5aa3b587768ede2
Enes Kaya ÖCAL <ekayaocal@hotmail.com>
2020-04-27 10:36:24 +0300

    missing file

---
+++ Source/RTS2/Data/FUnitNecessityRow.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine\DataTable.h"
+#include "RTS2\Prerequisites.h"
+#include "RTS2\Data\RTSPrimitiveResourceData.h"
+#include "FUnitNecessityRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct RTS2_API FUnitNecessity
+{
+	GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		TEnumAsByte<ENations> Nation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FRTSPrimitiveResourceData ResourceData;
+
+};
+
+USTRUCT(BlueprintType)
+struct FUnitNecessityRow
+    : public FTableRowBase
+{
+    GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		TEnumAsByte<EUnitTypes> UnitType;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		TArray<FUnitNecessity> Necessities;
+};
