c1587732e94f09cc7c820a40f20ab459bdb5525a
Enes Kaya ÖCAL <ekayaocal@hotmail.com>
2020-05-02 19:45:36 +0300

    Delegates template class has variadric support now.

--- Source/RTS2/Template/delegates.h
+++ Source/RTS2/Template/delegates.h
@@ -2,80 +2,85 @@
 
 #define MAX_LISTENER_COUNT 10
 
-template <typename P>
+template <typename ...P>
 class delegate
 {
    public:
-     delegate():
-        ListenerCount(0)
-     {
-         memset(object_ptrs, 0, sizeof(void*) * MAX_LISTENER_COUNT);
-         memset(stub_ptr, 0, sizeof(stub_type) * MAX_LISTENER_COUNT);
-     }
-
-    template <class T, void (T::*TMethod)(P)>
-    void add(T* object_ptr)
+    delegate():
+       ListenerCount(0)
     {
-         for(int i = 0; i < MAX_LISTENER_COUNT; i++)
-         {
-             if(object_ptrs[i] == nullptr)
+        memset(object_ptrs, 0, sizeof(void*) * MAX_LISTENER_COUNT);
+        memset(stub_ptr, 0, sizeof(stub_type) * MAX_LISTENER_COUNT);
+    }
+
+   template <class T, void (T::*TMethod)(P...)>
+   void add(T* object_ptr)
+   {
+        for(int i = 0; i < MAX_LISTENER_COUNT; i++)
+        {
+            if(object_ptrs[i] == nullptr)
+            {
+                object_ptrs[i] = object_ptr;
+                stub_ptr[i] = &method_stub<T, TMethod>;
+                ListenerCount++;
+                return;
+            }
+        }
+
+        // CANT ADD LISTENER TO LIST!
+   }
+
+   template <class T, void (T::*TMethod)(P...)>
+   void remove(T* object_ptr)
+   {
+        for(int i = 0; i < MAX_LISTENER_COUNT; i++)
+        {
+            if(object_ptrs[i] != nullptr && object_ptrs[i] == object_ptr)
+            {
+                object_ptrs[i] = nullptr;
+                stub_ptr[i] = nullptr;
+                ListenerCount--;
+                return;
+            }
+        }
+
+        // CANT FIND LISTENER ON LIST!
+   }
+
+   void operator()(P... a1) const
+   {
+        for(int i = 0; i < MAX_LISTENER_COUNT; i++)
+        {
+             if(object_ptrs[i] != nullptr)
              {
-                 object_ptrs[i] = object_ptr;
-                 stub_ptr[i] = &method_stub<T, TMethod>;
-                 return;
+                 (*stub_ptr[i])(object_ptrs[i], a1...);
              }
-         }
+        }
+   }
 
-         // CANT ADD LISTENER TO LIST!
-    }
+   void clear()
+   {
+       for(int i = 0; i < MAX_LISTENER_COUNT; i++)
+       {
+            object_ptrs[i] = nullptr;
+            stub_ptr[i] = nullptr;
+       }
+               
+       ListenerCount = 0;
+   }
 
-    template <class T, void (T::*TMethod)(P)>
-    void remove(T* object_ptr)
-    {
-         for(int i = 0; i < MAX_LISTENER_COUNT; i++)
-         {
-             if(object_ptrs[i] != nullptr && object_ptrs[i] == object_ptr)
-             {
-                 object_ptrs[i] = nullptr;
-                 stub_ptr[i] = nullptr;
-                 return;
-             }
-         }
-
-         // CANT FIND LISTENER ON LIST!
-    }
-    
-    template <class T, void (T::*TMethod)(P)>
-    static delegate from_method(T* object_ptr)
-    {
-        delegate d;
-        d.object_ptr = object_ptr;
-        d.stub_ptr = &method_stub<T, TMethod>; // #1
-        return d;
-    }
-
-    void operator()(P a1) const
-    {
-         for(int i = 0; i < MAX_LISTENER_COUNT; i++)
-         {
-              if(object_ptrs[i] != nullptr)
-              {
-                  (*stub_ptr[i])(object_ptrs[i], a1);
-              }
-         }
-    }
-
+    int ListenerCount;
 private:
-    typedef void (*stub_type)(void* object_ptr, int);
+    
+    typedef void (*stub_type)(void* object_ptr, P...);
     void* object_ptrs[MAX_LISTENER_COUNT];
     stub_type stub_ptr[MAX_LISTENER_COUNT];
-    int ListenerCount;
     
-    template <class T, void (T::*TMethod)(int)>
-    static void method_stub(void* object_ptr, P a1)
+    template <class T, void (T::*TMethod)(P...)>
+    static void method_stub(void* object_ptr, P... a1)
     {
         T* p = static_cast<T*>(object_ptr);
-        return (p->*TMethod)(a1); // #2
+        return (p->*TMethod)(a1...); // #2
     }
 
 
