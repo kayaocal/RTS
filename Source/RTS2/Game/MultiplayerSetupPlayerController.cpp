83bffc3db48f1f9b83e4d222d0e46a8b688c28da
Enes Kaya ÖCAL <ekayaocal@hotmail.com>
2020-05-14 20:20:28 +0300

    Compile error fixed

--- Source/RTS2/Game/MultiplayerSetupPlayerController.cpp
+++ Source/RTS2/Game/MultiplayerSetupPlayerController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Engine/Classes/Kismet/GameplayStatics.h"
 #include "MultiplayerSetupPlayerController.h"
+#include "Engine/Classes/Kismet/GameplayStatics.h"
 #include "RTS2/Prerequisites.h"
 #include "RTS2/GameMode/MultiplayerLobbyGameMode.h"
 
