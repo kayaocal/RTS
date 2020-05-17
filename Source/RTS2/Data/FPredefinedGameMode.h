f12eb51171ded5d8293f81230e91dd9e149408e9
Enes Kaya ÖCAL <ekayaocal@hotmail.com>
2020-05-18 00:09:43 +0300

    Missing FPredefinedGameMode.h

--- Source/RTS2/Data/FPredefinedGameMode.h
+++ Source/RTS2/Data/FPredefinedGameMode.h
@@ -1,6 +1,32 @@
 #pragma once
 
-struct FPredefinedGameMode
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "RTS2/Game/RTSGameMode.h"
+#include "FPredefinedGameMode.generated.h"
+
+USTRUCT(BlueprintType)
+struct RTS2_API FGameModeHolder
 {
+	GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FString Name;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FRTSGameMode Mode;
+
+};
+
+USTRUCT(BlueprintType)
+struct RTS2_API FPredefinedGameMode
+    : public FTableRowBase
+{
+    GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int ActiveGameModeIndex;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FGameModeHolder> GameModes;
 };
